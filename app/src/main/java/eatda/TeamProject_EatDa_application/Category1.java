015cdc5bc138d7fec21061f52848f36b74093cda
jungin <jungini0000@naver.com>
2021-12-02 15:44:29 +0900

    카테고리1 gridview추가

--- app/src/main/java/eatda/TeamProject_EatDa_application/Category1.java
+++ app/src/main/java/eatda/TeamProject_EatDa_application/Category1.java
@@ -1,6 +1,7 @@
 package eatda.TeamProject_EatDa_application;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,9 @@ public class Category1 extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category1);
+
+        final GridView gv1 = (GridView) findViewById(R.id.gv1);
+        MyGridAdapter gridAdapter = new MyGridAdapter(this);
+        gv1.setAdapter(gridAdapter);
     }
 }

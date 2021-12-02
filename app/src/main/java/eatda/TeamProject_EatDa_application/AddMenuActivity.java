582655853c7d5e4de31d74a02547564a3fbddb40
jungin <jungini0000@naver.com>
2021-12-02 11:23:57 +0900

    일단 커밋

--- app/src/main/java/eatda/TeamProject_EatDa_application/AddMenuActivity.java
+++ app/src/main/java/eatda/TeamProject_EatDa_application/AddMenuActivity.java
@@ -1,6 +1,10 @@
 package eatda.TeamProject_EatDa_application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +14,31 @@ public class AddMenuActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_menu);
+
+        ImageButton cat1 = (ImageButton) findViewById(R.id.imageButton1);
+        ImageButton cat2 = (ImageButton) findViewById(R.id.imageButton2);
+        ImageButton cat3 = (ImageButton) findViewById(R.id.imageButton3);
+
+        cat1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddMenuActivity.this, Category1.class);
+                startActivity(intent);
+            }
+        });
+        cat2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddMenuActivity.this, Category2.class);
+                startActivity(intent);
+            }
+        });
+        cat3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddMenuActivity.this, Category3.class);
+                startActivity(intent);
+            }
+        });
     }
 }

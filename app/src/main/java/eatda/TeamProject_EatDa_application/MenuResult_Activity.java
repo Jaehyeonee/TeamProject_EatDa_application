5db10df5cc55e6acfbd4c789884b22306f9eb33a
jaehyeonee <anjaehyeon0412@gmail.com>
2021-12-02 17:07:31 +0900

    레시피 보여주는 activity 생성 중
    이미지 가져오기 못함

---
+++ app/src/main/java/eatda/TeamProject_EatDa_application/MenuResult_Activity.java
@@ -0,0 +1,44 @@
+package eatda.TeamProject_EatDa_application;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class MenuResult_Activity extends AppCompatActivity {
+
+    private ImageView menuImageview;
+    private int imageResId;
+    private ArrayList<Dictionary_MyMenu> mItemList;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_result);
+
+        menuImageview=findViewById(R.id.addedMyImageView);  //메뉴 이미지 보여주기
+        String strTitle ="";
+
+
+
+        Bundle extras = getIntent().getExtras();
+
+        strTitle = extras.getString("strTitle");
+
+        TextView textView = (TextView)findViewById(R.id.menu_titleTextview) ;
+
+        String strMenuTitle = strTitle;
+        textView.setText(strMenuTitle);
+
+
+    }
+    public void setImageResId(int a_imageResId){
+        imageResId = a_imageResId;
+    }
+
+    }
+}

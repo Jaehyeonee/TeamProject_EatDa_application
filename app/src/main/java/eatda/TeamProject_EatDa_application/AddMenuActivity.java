740954e0f256f09b415a44efa95b101298800588
jungin <jungini0000@naver.com>
2021-12-02 12:50:34 +0900

    수정 1차

--- app/src/main/java/eatda/TeamProject_EatDa_application/AddMenuActivity.java
+++ app/src/main/java/eatda/TeamProject_EatDa_application/AddMenuActivity.java
@@ -5,40 +5,64 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddMenuActivity extends AppCompatActivity {
+
+    private static final int FROM_CAMERA = 0;
+    private static final int FROM_ALBUM = 1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_menu);
 
-        ImageButton cat1 = (ImageButton) findViewById(R.id.imageButton1);
-        ImageButton cat2 = (ImageButton) findViewById(R.id.imageButton2);
-        ImageButton cat3 = (ImageButton) findViewById(R.id.imageButton3);
+        tedPermission();
 
-        cat1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AddMenuActivity.this, Category1.class);
-                startActivity(intent);
-            }
-        });
-        cat2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AddMenuActivity.this, Category2.class);
-                startActivity(intent);
-            }
-        });
-        cat3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(AddMenuActivity.this, Category3.class);
-                startActivity(intent);
-            }
-        });
+        ImageView addImage = (ImageView) this.findViewById(R.id.addImageView);
+        ImageButton addButton = (ImageButton) this.findViewById(R.id.addButton);
+
+        addButton.setOnClickListener(this);
     }
+
+
+
+    //카메라에서 사진 촬영
+    public void doTakePhotoAction(){
+
+    }
+    //앨범에서 이미지 가져오기
+    public void  doTakeAlbumAction(){
+        //앨범 호출
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+        startActivityForResult(intent, FROM_ALBUM);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode != RESULT_OK)
+            return;
+        switch (requestCode){
+            case FROM_ALBUM:{
+
+            }
+            case FROM_CAMERA:{
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                //intent.setDataAndType(, "image/*");
+            }
+        }
+
+
+    }
+    public void onClick(View v){
+
+    }
+
 }

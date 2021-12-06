6ade7eaf8faeac768360456fabc1bc93e544e9f7
jungin <jungini0000@naver.com>
2021-12-06 23:31:23 +0900

    이미지 업로드

--- app/src/main/java/eatda/TeamProject_EatDa_application/AddMenuActivity.java
+++ app/src/main/java/eatda/TeamProject_EatDa_application/AddMenuActivity.java
@@ -1,49 +1,141 @@
 package eatda.TeamProject_EatDa_application;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
-public class AddMenuActivity extends AppCompatActivity {
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
+public class AddMenuActivity extends AppCompatActivity implements  View.OnClickListener{
 
     private static final int FROM_CAMERA = 0;
     private static final int FROM_ALBUM = 1;
-    Button addimgbtn;
+    private static final int CROP_FROM_iMAGE = 2;
+
+    //private static final int REQUEST_CODE = 0;
+    private ImageView photo;
+    private int id_view;
+    private String absoultePath;
+    private Uri mImageCaptureUri;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_menu);
+        checkSelfPermission();
 
-        //tedPermission();
+        photo = (ImageView)this.findViewById(R.id.addImageView);
+        Button addImagebtn = (Button) this.findViewById(R.id.addButton);
+        addImagebtn.setOnClickListener(this);
 
-        //ImageView addImage = (ImageView) this.findViewById(R.id.addImageView);
-        //ImageButton addButton = (ImageButton) this.findViewById(R.id.addButton);
-
-        //addimgbtn.setBackground(R.drawable.imgaddbtn_ripple);
-        //addButton.setOnClickListener(this);
+        /*photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });*/
     }
 
+    //접근 권한 받기
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            int length = permissions.length;
+            for (int i = 0; i < length; i++) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED)
+                    Log.d("AddMenuActivity", "권한허용 : " + permissions[i]);
+            }
+        }
+    }
 
-    //카메라에서 사진 촬영
-    public void doTakePhotoAction(){
+    public void checkSelfPermission() {
+        String temp = "";
+        //파일 읽기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.READ_EXTERNAL_STORAGE + " ";
+        }
+
+        //파일 쓰기 권한 확인
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            temp += Manifest.permission.WRITE_EXTERNAL_STORAGE + " ";
+        }
+
+        if (TextUtils.isEmpty(temp) == false) {
+            // 권한 요청
+            ActivityCompat.requestPermissions(this, temp.trim().split(" "), 1);
+        } else {
+            // 모두 허용 상태
+            Toast.makeText(this, "권한을 모두 허용", Toast.LENGTH_SHORT).show();
+        }
+    }
+    /**
+
+     * 카메라에서 사진 촬영
+
+     */
+
+    public void doTakePhotoAction() // 카메라 촬영 후 이미지 가져오기
+
+    {
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // 임시로 사용할 파일의 경로를 생성
+        String url = "tmp_" + String.valueOf(System.currentTimeMillis()) + ".jpg";
+        mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), url));
+
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+
+        startActivityForResult(intent, FROM_CAMERA);
 
     }
-    //앨범에서 이미지 가져오기
-    public void  doTakeAlbumAction(){
-        //앨범 호출
+    /**
+
+     * 앨범에서 이미지 가져오기
+
+     */
+
+    public void doTakeAlbumAction() // 앨범에서 이미지 가져오기
+
+    {
+        // 앨범 호출
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
         startActivityForResult(intent, FROM_ALBUM);
+
     }
 
     @Override
@@ -54,18 +146,145 @@ public class AddMenuActivity extends AppCompatActivity {
             return;
         switch (requestCode){
             case FROM_ALBUM:{
-
+                mImageCaptureUri = data.getData();
+                Log.d("SmartWheel",mImageCaptureUri.getPath().toString());
             }
-            case FROM_CAMERA:{
+
+            case FROM_CAMERA:
+            {
                 Intent intent = new Intent("com.android.camera.action.CROP");
-                //intent.setDataAndType(, "image/*");
+                intent.setDataAndType(mImageCaptureUri, "image/*");
+                // CROP할 이미지를 200*200 크기로 저장
+                intent.putExtra("outputX", 200); // CROP한 이미지의 x축 크기
+                intent.putExtra("outputY", 200); // CROP한 이미지의 y축 크기
+                intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
+                intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_iMAGE); // CROP_FROM_CAMERA case문 이동
+                break;
+            }
+            case CROP_FROM_iMAGE:
+            {
+                if (resultCode!=RESULT_OK){
+                    return;
+                }
+                final Bundle extras = data.getExtras();
+                //crop된 이미지를 저장하기 위한 파일 경로
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath()+
+                        "/SmartWheel/"+System.currentTimeMillis()+".jpg";
+                if(extras != null){
+                    Bitmap bitmap = extras.getParcelable("data");
+                    photo.setImageBitmap(bitmap);
+                    //crop된 이미지를 앨범에 저장
+                    storeCropImage(bitmap, filePath);
+                    absoultePath = filePath;
+                    break;
+                }
+                File f = new File(mImageCaptureUri.getPath());
+                if (f.exists()){
+                    f.delete();
+                }
             }
         }
 
+    }
+
+    @Override
+    public void onClick(View view) {
+        id_view = view.getId();
+        if (view.getId()==R.id.addButton){
+            DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    doTakePhotoAction();
+                }
+            };
+            DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    doTakeAlbumAction();
+                }
+            };
+            DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            };
+            new AlertDialog.Builder(this)
+                    .setTitle("업로드할 이미지 선택")
+                    .setPositiveButton("사진촬영",cameraListener)
+                    .setNeutralButton("앨범 선택",albumListener)
+                    .setNegativeButton("취소",cancelListener)
+                    .show();
+
+        }
 
     }
-    public void onClick(View v){
+    //bitmap 저장하는 부분
+    private void storeCropImage(Bitmap bitmap, String filePath){
+        String dirPath = Environment.getExternalStorageDirectory().getAbsolutePath()+"/SmartWheel";
+        File directory_SmartWheel = new File(dirPath);
 
+
+        if (!directory_SmartWheel.exists()) // SmartWheel 디렉터리에 폴더가 없다면 (새로 이미지를 저장할 경우에 속한다.)
+
+            directory_SmartWheel.mkdir();
+
+
+        File copyFile = new File(filePath);
+
+        BufferedOutputStream out = null;
+
+
+        try {
+
+
+            copyFile.createNewFile();
+
+            out = new BufferedOutputStream(new FileOutputStream(copyFile));
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+
+
+            // sendBroadcast를 통해 Crop된 사진을 앨범에 보이도록 갱신한다.
+
+            sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+
+                    Uri.fromFile(copyFile)));
+
+
+            out.flush();
+
+            out.close();
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
     }
+
+    /*@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    photo.setImageBitmap(img);
+                } catch (Exception e) {
+
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
+    }*/
+
 
 }
